bc9431fff7c269e701a968962d7331c8fafdf3ef
Denny Piza <dpiza@student.42sp.org.br>
2021-11-14 23:52:55 +0100

    removes quick_sort

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 02:42:14 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/14 23:47:25 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/14 23:51:32 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,33 +45,6 @@ void	push_n_b(t_stack *stack, int n)
 		push_b(stack);
 }
 
-void	quicker_sort(t_stack *stack)
-{
-	int	min;
-	int	b_count;
-	int	i;
-	int	j;
-
-	j = 0;
-	min = INT_MIN;
-	while (!(is_sorted(stack->a)))
-	{
-		b_count = select_push(stack, min, ft_atoi(stack->interval[j]));
-		i = b_count;
-		while (i--)
-		{
-			select_sort(stack);
-			push_a(stack);
-		}
-		if (!is_sorted(stack->a))
-			push_n_b(stack, b_count);
-		else
-			push_all(stack);
-		min = ft_atoi(stack->interval[j]);
-		j++;
-	}
-}
-
 int	push_swap(t_stack *stack)
 {
 	if (ft_stacklen(stack->a) <= 3)
@@ -90,6 +63,6 @@ int	push_swap(t_stack *stack)
 		}
 	}
 	else
-		quicker_sort(stack);
+		quick_sort(stack);
 	return (0);
 }
