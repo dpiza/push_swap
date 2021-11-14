da2460d3dfb3e6e8781f36faa0a5d1b5a4b72ce5
Denny Piza <dpiza@student.42sp.org.br>
2021-11-14 20:04:46 +0100

    organized by file

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 18:40:03 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/14 02:33:06 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/14 20:04:11 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "../libft/includes/ft_printf.h"
 # include "defines.h"
 
-typedef struct	s_stack {
+typedef struct s_stack {
 	char	**a;
 	char	**b;
 	char	**interval;
@@ -30,9 +30,17 @@ int		push_swap(t_stack *stack);
 int		is_sorted(char **stack);
 int		is_sorted_b(char **stack);
 
-void	print_interval(t_stack *stack);
-void	print_stacks(t_stack *stack);
+int		check_duplicates(char **stack);
+char	**fill_stack(int argc, char **argv);
+char	**fill_interval(char **a);
+void	bubble_sort(char **arr);
 
+void	print_stacks(t_stack *stack);
+void	print_interval(t_stack *stack);
+
+void	top_add(char **stack, char *member);
+void	top_rmv(char **stack);
+int		ft_stacklen(char **s);
 void	add_op(char	**stack_op, char *op);
 
 void	swap_a(t_stack *stack);
@@ -47,9 +55,7 @@ void	reverse_a(t_stack *stack);
 void	reverse_b(t_stack *stack);
 void	reverse_ab(t_stack *stack);
 
-int		ft_stacklen(char **s);
-void	top_rmv(char **stack);
-void	top_add(char **stack, char *member);
+void	free_stack(t_stack *stack);
 
 void	exit_err(char *str);
 
