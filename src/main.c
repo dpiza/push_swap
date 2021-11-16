f2300e2695226e3fe75475eb03d629856eae0e4e
Denny Piza <dpiza@student.42sp.org.br>
2021-11-16 14:38:45 +0100

    fixes return for execution without argument

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dpiza <dpiza@student.42sp.org.br>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 18:36:53 by dpiza             #+#    #+#             */
-/*   Updated: 2021/11/15 00:52:44 by dpiza            ###   ########.fr       */
+/*   Updated: 2021/11/16 14:37:38 by dpiza            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv)
 	t_stack	stack;
 
 	if (argc < 2)
-		exit_err("Error\n");
+		exit(0);
 	stack.a = fill_stack(argc, argv);
 	if (check_duplicates(stack.a))
 		exit_err("Error\n");
@@ -25,10 +25,7 @@ int	main(int argc, char **argv)
 	stack.b = (char **)ft_calloc((argc + 1), sizeof(char *));
 	stack.in = 0;
 	stack.op = ft_strdup("");
-	// print_stacks(&stack);
-	// print_interval(&stack);
 	push_swap(&stack);
-	// print_stacks(&stack);
 	ft_printf("%s", stack.op);
 	free_stack(&stack);
 	return (0);
